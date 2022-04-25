9b2ed12391c70d2cff8d1c48c4619b6bb78207a6
serwol <adamal_serg@tut.by>
2022-04-25 22:06:48 +0300

    18

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 
 app = Flask(__name__)
 
-@app.route("/")
 
+@app.route("/")
 
 def hello():
     try:
@@ -20,10 +20,9 @@ def hello():
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+    return html.format(name=os.getenv("NAME", "world"), 
+    hostname=socket.gethostname(), visits=visits)
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
-
-    
