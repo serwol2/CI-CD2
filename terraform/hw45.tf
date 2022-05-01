f0d08a73185cdfe0f22948aec33144a3473ae345
serwol <adamal_serg@tut.by>
2022-05-01 15:13:01 +0300

    21

--- terraform/hw45.tf
+++ terraform/hw45.tf
@@ -75,7 +75,7 @@ resource "aws_instance" "for-docker-hw45" {
 
 output "ec2instance" {
   description = "Type in your browser:"
-  value = "http://"+aws_instance.for-docker-hw45.public_ip+":4000"
+  value = "http://${aws_instance.for-docker-hw45.public_ip}:4000"
  }
 
 resource "aws_secretsmanager_secret" "hw45-github-token" {
