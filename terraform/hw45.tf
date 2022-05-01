c9afe2469bc45ad8ae4cecb039f1d4b9c72f4eef
serwol <adamal_serg@tut.by>
2022-05-01 11:44:55 +0300

    6

--- terraform/hw45.tf
+++ terraform/hw45.tf
@@ -28,56 +28,65 @@ provider "aws" {
 
 }
 
-resource "aws_security_group" "my-sg-hw45" {
-  name        = "my-sg-hw45"
-  description = "Traffic 80 and 22"
+# resource "aws_security_group" "my-sg-hw45" {
+#   name        = "my-sg-hw45"
+#   description = "Traffic 80 and 22"
 
-  ingress {
-    description      = "22"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description      = "22"
+#     from_port        = 22
+#     to_port          = 22
+#     protocol         = "tcp"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#   }
 
-  ingress {
-    description      = "80"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description      = "80"
+#     from_port        = 80
+#     to_port          = 80
+#     protocol         = "tcp"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#   }
  
-  ingress {
-    description      = "4000"
-    from_port        = 4000
-    to_port          = 4000
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description      = "4000"
+#     from_port        = 4000
+#     to_port          = 4000
+#     protocol         = "tcp"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
 
+# }
+
+# resource "aws_instance" "for-docker-hw45" {
+#   ami           = "ami-04505e74c0741db8d"
+#   instance_type = "t2.micro"
+#   key_name = "mykeypairsergey"
+#   security_groups = [aws_security_group.my-sg-hw45.name]
+#   # user_data = "${file("inst-nginx.sh")}" 
+#   tags = {
+#     Name = "For-docker-hw45"
+#   }
+
+# }
+
+# output "ec2instance" {
+#   value = aws_instance.for-docker-hw45.public_ip
+# }
+
+resource "aws_secretsmanager_secret" "hw45-github-token" {
+   name = "github_token"
 }
 
-resource "aws_instance" "for-docker-hw45" {
-  ami           = "ami-04505e74c0741db8d"
-  instance_type = "t2.micro"
-  key_name = "mykeypairsergey"
-  security_groups = [aws_security_group.my-sg-hw45.name]
-  # user_data = "${file("inst-nginx.sh")}" 
-  tags = {
-    Name = "For-docker-hw45"
-  }
-
-}
-
-output "ec2instance" {
-  value = aws_instance.for-docker-hw45.public_ip
+resource "aws_secretsmanager_secret_version" "github_token_ver" {
+  secret_id     = aws_secretsmanager_secret.hw45-github-token.id
+  secret_string = "${GITH_TOKEN}"
 }
